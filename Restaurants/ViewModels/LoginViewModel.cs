802dbdc9b52c4c4aff1b74ae877864d00d935e20
Vladimir Kopytin <kopytinv97@bk.ru>
2017-04-22 00:24:27 +0300

    LoginViewModel

---
+++ Restaurants/ViewModels/LoginViewModel.cs
@@ -0,0 +1,85 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Restaurants.Core.Services;
+using Restaurants.Helpers;
+using System.Windows;
+
+namespace Restaurants.ViewModels
+{
+    class LoginViewModel : ViewModelBase
+    {
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+            RegisterCommand = new RelayCommand(Register);
+
+            Username = "";
+            Password = "";
+        }
+
+        string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    RaisePropertyChanged("Username");
+                }
+            }
+        }
+
+        string _password;
+        public string Password
+        {
+            get
+            {
+                return new string('*', _password.Length);
+            }
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    RaisePropertyChanged("Password");
+                }
+            }
+        }
+
+        public RelayCommand LoginCommand { get; set; }
+        async void Login(object parameter)
+        {
+            if (!await AuthenticationService.Login(Username, Password))
+            {
+                MessageBox.Show("Неверное имя пользователя или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            new MainWindow().Show();
+            NotifyWindowToClose();
+        }
+
+        public RelayCommand RegisterCommand { get; set; }
+        async void Register(object parameter)
+        {
+            if (!await AuthenticationService.Register(Username, Password))
+            {
+                MessageBox.Show("Имя уже занято", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            new MainWindow().Show();
+            NotifyWindowToClose();
+        }
+
+        public void NotifyWindowToClose()
+        {
+            Messenger.Default.Send(new NotificationMessage(this, "CloseWindowsBoundToMe"));
+        }
+
+    }
+}
--- Restaurants/Views/LoginView.xaml.cs
+++ Restaurants/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Restaurants.Core.Services;
+﻿using GalaSoft.MvvmLight.Messaging;
+using Restaurants.Core.Services;
 using System.Windows;
 
 namespace Restaurants.Views
@@ -11,40 +12,26 @@ namespace Restaurants.Views
         public LoginView()
         {
             InitializeComponent();
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private async void RegisterBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!await AuthenticationService.Register(UsernameTxt.Text, PasswordTxt.Password))
+            Messenger.Default.Register<NotificationMessage>(this, (nm) =>
             {
-                MessageBox.Show("Имя уже занято", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            new MainWindow().Show();
-            this.Close();
+                if (nm.Notification == "CloseWindowsBoundToMe")
+                {
+                    if (nm.Sender == this.DataContext)
+                        this.Close();
+                }
+            });
         }
 
-        private async void LoginBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!await AuthenticationService.Login(UsernameTxt.Text, PasswordTxt.Password))
-            {
-                MessageBox.Show("Неверное имя пользователя или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            new MainWindow().Show();
-            this.Close();
-        }
 
         private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
