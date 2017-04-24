1b5b29def8c5017832bf83c9cc9e8909a2607310
Vladimir Kopytin <kopytinv97@bk.ru>
2017-04-25 00:45:36 +0300

    splash

---
+++ Restaurants/Views/SplashView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurants.Views
+{
+    /// <summary>
+    /// Interaction logic for SplashView.xaml
+    /// </summary>
+    public partial class SplashView : Window
+    {
+        public SplashView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private async void SplashView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(3000);
+            this.Hide();
+            new LoginView().Show();
+        }
+    }
+}
